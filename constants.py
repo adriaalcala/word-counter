e8064f69f35f717b72183d6fd7f78e67f95d81e2
adriaalcala <adria.alcala@gmail.com>
2019-02-13 10:59:00 +0100

    Add logging and improve makefile (#2)

--- constants.py
+++ constants.py
@@ -7,3 +7,23 @@ DEFAULT_FILES = [
 ]
 
 DEFAULT_BUCKET = "apache-beam-samples"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {"format": "[%(asctime)s %(levelname)s:%(name)s]: %(message)s"},
+        "detailed": {
+            "format": "[%(asctime)s %(levelname)s:%(name)s.%(funcName)s:%(lineno)d]: %(message)s"  # noqa
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "DEBUG",
+            "formatter": "simple",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+    "loggers": {"weasyprint": {"level": "ERROR"}},
+}
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import logging
+import logging.config
 import re
 from argparse import ArgumentParser
 from collections import Counter
 
 from google.cloud import storage
 
-from constants import DEFAULT_BUCKET, DEFAULT_FILES, STOPCHARS
+from constants import DEFAULT_BUCKET, DEFAULT_FILES, LOGGING, STOPCHARS
 from report import create_response
+
+logger = logging.getLogger(__name__)
 
 
 def get_text_from_file(bucket_name, storage_file):
@@ -18,19 +22,25 @@ def get_text_from_file(bucket_name, storage_file):
 
 
 def get_counter_from_file(bucket_name, storage_file, sensitive=False):
+    logger.info(f"Downloading file {storage_file}")
     text = get_text_from_file(bucket_name, storage_file)
+    logger.info(f"File {storage_file} downloaded")
+    logger.info(f"Getting words for file {storage_file}")
     words = filter(None, re.split(STOPCHARS, text.decode("utf-8")))
     if not sensitive:
         words = (word.lower() for word in words)
+    logger.info(f"Counting words in file {storage_file}")
     return Counter(words)
 
 
 def main(bucket_name, files, sensitive=False, pdf_output=False, store_csv=False):
     words_counter = Counter([])
     for storage_file in files:
+        logger.info(f"Processing file {storage_file}")
         words_counter += get_counter_from_file(
             bucket_name, storage_file, sensitive=sensitive
         )
+    logger.info(f"Creating report")
     create_response(
         bucket_name, files, words_counter, pdf_output=pdf_output, store_csv=store_csv
     )
@@ -60,10 +70,15 @@ if __name__ == "__main__":
     parser.add_argument(
         "--store_csv", action="store_true", help="Store csv with results"
     )
-
+    parser.add_argument(
+        "--verbose", action="store_true", help="Log the process step by step"
+    )
     args = parser.parse_args()
     if not args.files:
         args.files = DEFAULT_FILES
+    if args.verbose:
+        logging.config.dictConfig(LOGGING)
+
     main(
         args.bucket_name,
         args.files,
